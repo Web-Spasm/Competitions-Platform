84054ab74d073fa57a36ef1bc04786517925e54e
Maia <maianeptune@gmail.com>
2024-11-23 16:20:07 -0400

    removed rank and ranking id from RankingHistory added it to Ranking

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -7,9 +7,5 @@ from .competition_moderator import *
 from .competition_team import *
 from .student_team import *
 from .notification import *
-<<<<<<< HEAD
 from .ranking import *
-=======
-from .rank import *
->>>>>>> 8057185 (Setup Rank and Ranking classes)
 from .ranking_history import *
