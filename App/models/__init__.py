dea92d2a4fc213c9fad38daee2085ea4791c0e96
Enadiz76 <zidtimo@gmail.com>
2024-11-23 15:42:21 -0400

    Setup Rank and Ranking classes

--- App/models/__init__.py
+++ App/models/__init__.py
@@ -7,13 +7,5 @@ from .competition_moderator import *
 from .competition_team import *
 from .student_team import *
 from .notification import *
-<<<<<<< HEAD
-<<<<<<< HEAD
-from .ranking import *
-=======
 from .rank import *
->>>>>>> 8057185 (Setup Rank and Ranking classes)
-=======
-from .ranking import *
->>>>>>> b134881 (removed rank and ranking id from RankingHistory added it to Ranking)
 from .ranking_history import *
