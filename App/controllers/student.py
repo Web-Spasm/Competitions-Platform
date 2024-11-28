7676a49b693889f39b14f43de74db9e0d28cc9f6
Maia <maianeptune@gmail.com>
2024-11-28 02:01:13 -0400

    unranked students history on calendar

--- App/controllers/student.py
+++ App/controllers/student.py
@@ -31,6 +31,7 @@ def get_student(id):
 def get_all_students():
     return Student.query.all()
 
+
 def get_all_students_json():
     students = Student.query.all()
     if not students:
@@ -88,6 +89,13 @@ def display_notifications(username):
 def update_rankings(competition):
     students = get_all_students()
     
+    #for unranked students
+    for student in students:
+        student_history = ranking_history.get_ranking_history_by_id(student.id)
+
+        if student.curr_rank == 0:
+            student_ranking = ranking.create_ranking(student_history.id, competition.id, 0 , 'gray' ,competition.date)
+    
     students.sort(key=lambda x: (x.rating_score, x.comp_count), reverse=True)
 
     leaderboard = []
@@ -108,6 +116,7 @@ def update_rankings(competition):
             count += 1
         
             student.curr_rank = curr_rank
+
             if student.prev_rank == 0:
                 message = f'RANK : {student.curr_rank}. Congratulations on your first rank!'
                 student_ranking = ranking.create_ranking(student_history.id, competition.id, curr_rank, 'green' ,competition.date)
