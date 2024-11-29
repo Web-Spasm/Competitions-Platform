465127ca7df4ec019213b4b6f7526313a5ec6f37
Maia Neptune <maianeptune@gmail.com>
2024-11-29 18:06:11 -0400

    finished unit tests for rank and ranking history, moderator add results debugging

--- App/controllers/moderator.py
+++ App/controllers/moderator.py
@@ -130,7 +130,7 @@ def add_results(mod_name, comp_name, team_name, score):
                         return comp_team
                     except Exception as e:
                         db.session.rollback()
-                        # print("Something went wrong: ", e)
+                        ("Something went wrong: ", e)
                         return None
     return None
 
